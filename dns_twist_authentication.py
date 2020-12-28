77108b3309601df4701f2f1a0acf5e53b4b04002
Sakshi1503 <38690949+Sakshi1503@users.noreply.github.com>
2020-12-28 21:46:58 +0530

    Add files via upload

--- dns_twist_authentication.py
+++ dns_twist_authentication.py
@@ -2,8 +2,8 @@ import connection
 import datetime
 import bcrypt
 
-emailID = input("Enter emailID")
-password = input("Enter password").encode('UTF-8')
+emailID = input("Enter emailID: ")
+password = input("Enter password: ").encode('UTF-8')
 
 #finding the mail ID from DB
 data = connection.mydb.user.find_one({"email":emailID})
@@ -11,7 +11,10 @@ data = connection.mydb.user.find_one({"email":emailID})
 #checking the encrypted password to get authenticated
 if bcrypt.checkpw(password,data['password']):
     print("Authenticated")
+    #updating the time-stamp
+    connection.mydb.user.find_one_and_update({"email" : emailID},{"$set":{"latest_login_time": datetime.datetime.now()}},upsert=True)
 else:
     print("Not Authenticated")
 
-
+#closing the connection
+connection.connect_close
